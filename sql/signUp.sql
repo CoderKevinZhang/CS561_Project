4fd2bcf2b44016fa0de1e3e89b2f5b15f3bac78c
ZhaoxiongYang <zhaoxiong.yang@gmail.com>
2018-10-08 18:18:11 -0700

    add encrypt process for sign up

--- sql/signUp.sql
+++ sql/signUp.sql
@@ -1,5 +1,7 @@
 
-INSERT INTO User_info (User_name, Password, User_role, Email, Phone) VALUES ('A', '123', 'Buyer', 'xx@gmail.com', '54122222');
+INSERT INTO User_info (User_name, Password, User_role, Email, Phone) 
+VALUES ('A', AES_ENCRYPT('123', UNHEX(SHA2('My secret passphrase',512))), 'Buyer', 'xx@gmail.com', '54122222');
+
 
 SELECT CASE ( SELECT COUNT(`User_name`) FROM `User_info` WHERE `User_name` = "A") 
 	WHEN 0 THEN CONCAT ('{"Code":'   ,  -1 , ','  '"msg":', '"Sign up Failed!"}')
