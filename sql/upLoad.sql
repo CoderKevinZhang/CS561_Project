4db9d1870ad2b1935029720cf89335b0979150ce
ZhaoxiongYang <zhaoxiong.yang@gmail.com>
2018-10-28 17:01:24 -0700

    Update sql/upLoad.sql

--- sql/upLoad.sql
+++ sql/upLoad.sql
@@ -1,12 +1,12 @@
-INSERT INTO `Houses2` (`User_id`, `Zipcode`, `Address`, `City`, `State`,`Price`,`Beds`, `Baths`, `Built`) 
+INSERT INTO `Houses2` (`User_id`, `Zipcode`, `Address`, `City`, `State`,`Price`,`Beds`, `Baths`, `Built`,`description`,`Space`,`Lot_space`) 
 VALUES(
 	(SELECT `User_id` FROM `User_info` WHERE `User_name` = 'yangz'),
-	'97730','1230 Nw Ridegewood Pl', 'Corvallis', 'Oregon', '100000',3,2,2017)
+	'97730','1230 Nw Ridegewood Pl', 'Corvallis', 'Oregon', '100000',3,2,2017,'nice house',500,2000)
 
 
-SELECT `House_id` FROM Houses2 ORDER BY `House_id` DESC LIMIT 1
+SELECT `House_id` FROM `Houses2` ORDER BY `House_id` DESC LIMIT 1
 
 INSERT INTO `Houses_image` (`House_id`, `Url`) 
 VALUES(
-	SELECT `House_id` FROM Houses2 ORDER BY `House_id` DESC LIMIT 1,
+	(SELECT `House_id` FROM Houses2 ORDER BY `House_id` DESC LIMIT 1),
 	'wwwwwwwwwwwwwwwwwwwww')
